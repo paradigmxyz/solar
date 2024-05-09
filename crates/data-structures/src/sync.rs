f3bec47ea084b1d6625b7ea11697aa42d779d014
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-05-09 16:43:40 +0200

    chore: remove parallel feature by making it always the default

---
+++ crates/data-structures/src/sync.rs
@@ -0,0 +1,14 @@
+pub use sync::*;
+
+mod sync {
+    pub use parking_lot::{
+        MappedMutexGuard as MappedLockGuard, MappedRwLockReadGuard as MappedReadGuard,
+        MappedRwLockWriteGuard as MappedWriteGuard, Mutex as Lock, RwLock,
+        RwLockReadGuard as ReadGuard, RwLockWriteGuard as WriteGuard,
+    };
+
+    pub use std::sync::{
+        atomic::{AtomicBool, AtomicU32, AtomicU64, AtomicUsize},
+        Arc as Lrc, OnceLock, Weak,
+    };
+}
