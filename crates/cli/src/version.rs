a69abc9eb4a9f0d021d2ddc86e06f3f86177ddc0
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-10-21 11:11:41 +0200

    chore: simplify version constants

--- crates/cli/src/version.rs
+++ crates/cli/src/version.rs
@@ -1,34 +1,3 @@
-/// The build profile name.
-pub const BUILD_PROFILE_NAME: &str = {
-    // https://stackoverflow.com/questions/73595435/how-to-get-profile-from-cargo-toml-in-build-rs-or-at-runtime
-    const OUT_DIR: &str = env!("OUT_DIR");
-    let unix_parts = const_format::str_split!(OUT_DIR, '/');
-    if unix_parts.len() >= 4 {
-        unix_parts[unix_parts.len() - 4]
-    } else {
-        let win_parts = const_format::str_split!(OUT_DIR, '\\');
-        win_parts[win_parts.len() - 4]
-    }
-};
-
-/// The latest version from Cargo.toml.
-pub const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-/// The full SHA of the latest commit.
-pub const VERGEN_GIT_SHA_LONG: &str = env!("VERGEN_GIT_SHA");
-
-/// The 8 character short SHA of the latest commit.
-pub const VERGEN_GIT_SHA: &str = const_format::str_index!(VERGEN_GIT_SHA_LONG, ..8);
-
-/// The build timestamp.
-pub const VERGEN_BUILD_TIMESTAMP: &str = env!("VERGEN_BUILD_TIMESTAMP");
-
-/// The target triple.
-pub const VERGEN_CARGO_TARGET_TRIPLE: &str = env!("VERGEN_CARGO_TARGET_TRIPLE");
-
-/// The build features.
-pub const VERGEN_CARGO_FEATURES: &str = env!("VERGEN_CARGO_FEATURES");
-
 /// The short version information.
 pub const SHORT_VERSION: &str = const_format::concatcp!(
     env!("CARGO_PKG_VERSION"),
@@ -36,7 +5,7 @@ pub const SHORT_VERSION: &str = const_format::concatcp!(
     " (",
     VERGEN_GIT_SHA,
     " ",
-    VERGEN_BUILD_TIMESTAMP,
+    env!("VERGEN_BUILD_TIMESTAMP"),
     ")",
 );
 
@@ -57,3 +26,22 @@ pub const LONG_VERSION: &str = const_format::concatcp!(
     "Build Profile: ",
     BUILD_PROFILE_NAME,
 );
+
+/// The build profile name.
+const BUILD_PROFILE_NAME: &str = {
+    // https://stackoverflow.com/questions/73595435/how-to-get-profile-from-cargo-toml-in-build-rs-or-at-runtime
+    const OUT_DIR: &str = env!("OUT_DIR");
+    let unix_parts = const_format::str_split!(OUT_DIR, '/');
+    if unix_parts.len() >= 4 {
+        unix_parts[unix_parts.len() - 4]
+    } else {
+        let win_parts = const_format::str_split!(OUT_DIR, '\\');
+        win_parts[win_parts.len() - 4]
+    }
+};
+
+/// The full SHA of the latest commit.
+const VERGEN_GIT_SHA_LONG: &str = env!("VERGEN_GIT_SHA");
+
+/// The 8 character short SHA of the latest commit.
+const VERGEN_GIT_SHA: &str = const_format::str_index!(VERGEN_GIT_SHA_LONG, ..8);
