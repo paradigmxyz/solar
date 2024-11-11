871ef68db592d188573475f56f42cf80ae8206df
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-11-11 16:01:28 +0700

    Add syntax test pragma solidity (#112)
    * Add syntax test for pragma version without Solidity
    
    * Fix

--- tests/ui/parser/pragma_unknown.sol
+++ tests/ui/parser/pragma_unknown.sol
@@ -9,3 +9,5 @@ pragma amogus 69;
 //~^ ERROR: unknown pragma
 pragma amogus 69 diwqbn9ru3b2q945 390ru31290r 0qjr09wadm;
 //~^ ERROR: unknown pragma
+pragma amogus 0.8.15;
+//~^ ERROR: only `solidity` is supported as a version pragma
