b830214c672f6666175443df3a5a8fe898baec7b
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-11-12 12:12:42 +0700

    AST validate check statement only in while/for loops (#111)
    * AST validate check statement only in while/for loops
    
    * Add integration test
    
    * Fix comments
    
    * Update error message

---
+++ tests/ui/parser/break_continue_outside_loop.sol
@@ -0,0 +1,21 @@
+contract C {
+    function f() {
+        break; //~ ERROR: "break" has to be in a "for" or "while" loop.
+        continue; //~ ERROR: "continue" has to be in a "for" or "while" loop.
+
+        for (uint256 i = 0; i < 10; i++) {
+            break;
+            continue;
+        }
+
+        while (true) {
+            break;
+            continue;
+        }
+
+        do {
+            break;
+            continue;
+        } while (true);
+    }
+}
