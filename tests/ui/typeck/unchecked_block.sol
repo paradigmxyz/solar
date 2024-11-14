c0c8541e7a52c44b5fea820d4a686d3b54c50f20
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-11-14 10:01:43 +0100

    test: add some more tests

--- tests/ui/typeck/unchecked_block.sol
+++ tests/ui/typeck/unchecked_block.sol
@@ -1,10 +1,19 @@
+function f() public {
+    unchecked {}
+
+    unchecked {
+        unchecked {} //~ ERROR: `unchecked` blocks cannot be nested
+        unchecked {} //~ ERROR: `unchecked` blocks cannot be nested
+    }
+}
+
 contract C {
-    function a() public {
+    function f() public {
         unchecked {}
 
         unchecked {
-            unchecked {} //~ ERROR: 'unchecked' blocks cannot be nested
-            unchecked {} //~ ERROR: 'unchecked' blocks cannot be nested
+            unchecked {} //~ ERROR: `unchecked` blocks cannot be nested
+            unchecked {} //~ ERROR: `unchecked` blocks cannot be nested
         }
     }
 }
