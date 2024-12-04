fe935420ce993d8887f641f1efadce235e2a3414
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-12-04 18:23:40 +0100

    perf: keep skipping comments

--- crates/parse/src/lexer/mod.rs
+++ crates/parse/src/lexer/mod.rs
@@ -94,6 +94,9 @@ impl<'sess, 'src> Lexer<'sess, 'src> {
             if token.is_eof() {
                 break;
             }
+            if token.is_comment() {
+                continue;
+            }
             tokens.push(token);
         }
         trace!(
