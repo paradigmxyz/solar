213120de6b491e20b0a31cc6cd439ab6f7fc54e4
TilakMaddy <tilak.madichetti@gmail.com>
2024-12-09 00:26:31 +0530

    fix: added error annotation for var decl as body of loop

--- tests/ui/resolve/loops.sol
+++ tests/ui/resolve/loops.sol
@@ -11,16 +11,22 @@ function funky() {
     for (; i++ < 50;) continue;
 
     while (a == 0) uint a = 0; //~ ERROR: unresolved symbol
+    //~^ ERROR: variable declaration statements are not allowed as the body of a loop (for, while, do while), meaning they must be inside of a block
+
     a; //~ ERROR: unresolved symbol
     while (b == 0) { uint b = 0; } //~ ERROR: unresolved symbol
     b; //~ ERROR: unresolved symbol
 
     do uint c; while (c == 0); //~ ERROR: unresolved symbol
+    //~^ ERROR: variable declaration statements are not allowed as the body of a loop (for, while, do while), meaning they must be inside of a block
+
     c; //~ ERROR: unresolved symbol
     do { uint d; } while (d == 0); //~ ERROR: unresolved symbol
     d; //~ ERROR: unresolved symbol
 
     for (; false; e++) uint e; //~ ERROR: unresolved symbol
+    //~^ ERROR: variable declaration statements are not allowed as the body of a loop (for, while, do while), meaning they must be inside of a block
+
     e; //~ ERROR: unresolved symbol
     for (; false; f++) { uint f; } //~ ERROR: unresolved symbol
     f; //~ ERROR: unresolved symbol
