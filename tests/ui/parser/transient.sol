08bd2e66f098b9f2592a717d2b233b70ae7e4186
TilakMaddy <tilak.madichetti@gmail.com>
2024-12-10 13:12:38 +0530

    fix: add back new lines

--- tests/ui/parser/transient.sol
+++ tests/ui/parser/transient.sol
@@ -18,13 +18,17 @@ contract E {
     function f(uint256 transient) external { 
         transient = 3;
     }
+    
     function f2(uint256 transient, bool) external { 
         transient = 4;
     }
+
     function g(uint256 transient transient) external { //~ ERROR: data location can only be specified for array, struct or mapping types
         transient = 5;
     }
+
     function g2(uint256[] transient transient) external { //~ ERROR: invalid data location
         transient = 5;
     }
+
   }
