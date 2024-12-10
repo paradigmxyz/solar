f650db96ba9ac60b1ef06d0e8bc78695af303492
TilakMaddy <tilak.madichetti@gmail.com>
2024-12-11 00:08:13 +0530

    fix: cleanup

--- tests/ui/resolve/loops.sol
+++ tests/ui/resolve/loops.sol
@@ -10,30 +10,23 @@ function funky() {
     for (; i < 40; i++) continue;
     for (; i++ < 50; ) continue;
 
-    while (a == 0) { //~ ERROR: unresolved symbol 
-        uint a = 0;
-    }
+    while (a == 0) { uint a = 0; } //~ ERROR: unresolved symbol 
     a; //~ ERROR: unresolved symbol
-    while (b == 0) { //~ ERROR: unresolved symbol
-        uint b = 0;
-    } 
+    while (b == 0) { uint b = 0; } //~ ERROR: unresolved symbol
     b; //~ ERROR: unresolved symbol
 
     do { uint c; } while (c == 0); //~ ERROR: unresolved symbol
     c; //~ ERROR: unresolved symbol
-    do {
-        uint d;
-    } while (d == 0); //~ ERROR: unresolved symbol
+    do { uint d; } while (d == 0); //~ ERROR: unresolved symbol
     d; //~ ERROR: unresolved symbol
 
     for (; false; e++) { uint e; } //~ ERROR: unresolved symbol
     e; //~ ERROR: unresolved symbol
-    for (; false; f++) {//~ ERROR: unresolved symbol
-
-        uint f;
-    }     f; //~ ERROR: unresolved symbol
+    for (; false; f++) { uint f; }//~ ERROR: unresolved symbol
+    f; //~ ERROR: unresolved symbol
     for (uint g; false; g++) {
         g;
     }
     g; //~ ERROR: unresolved symbol
 }
+
