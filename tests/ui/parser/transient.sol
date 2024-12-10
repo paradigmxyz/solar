7665c9607b4b8f270bd1981db6e303b582773921
TilakMaddy <tilak.madichetti@gmail.com>
2024-12-10 12:21:15 +0530

    fix: change visiblity for transient test cases

--- tests/ui/parser/transient.sol
+++ tests/ui/parser/transient.sol
@@ -15,16 +15,16 @@ contract D {
 }
 
 contract E {
-    function f(uint256 transient) { //~ ERROR: no visibility specified
+    function f(uint256 transient) external { 
         transient = 3;
     }
-    function f2(uint256 transient, bool) { //~ ERROR: no visibility specified
+    function f2(uint256 transient, bool) external { 
         transient = 4;
     }
-    function g(uint256 transient transient) public { //~ ERROR: data location can only be specified for array, struct or mapping types
+    function g(uint256 transient transient) external { //~ ERROR: data location can only be specified for array, struct or mapping types
         transient = 5;
     }
-    function g2(uint256[] transient transient) public { //~ ERROR: invalid data location
+    function g2(uint256[] transient transient) external { //~ ERROR: invalid data location
         transient = 5;
     }
   }
