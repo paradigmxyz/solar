9ffe364a5dc3fa09f88818cd1eccb601768d751c
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-01-12 08:58:25 +0100

    fix: allow storage in modifiers (#185)

--- tests/ui/typeck/library_mappings.sol
+++ tests/ui/typeck/library_mappings.sol
@@ -1,5 +1,6 @@
-// contract L {
-//     function f(mapping(uint=>uint) storage x, mapping(uint=>uint) storage y) internal {
-//         // x = y;
-//     }
-// }
+contract L {
+    function f(mapping(uint=>uint) storage x, mapping(uint=>uint) storage y) internal {
+        // TODO: disallow assignment
+        x = y;
+    }
+}
