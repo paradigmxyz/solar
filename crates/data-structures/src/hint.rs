8c095a1d93cd06c4eb74219feda01474cfac159b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-04-28 22:31:04 +0200

    chore: update hint fns (#289)

--- crates/data-structures/src/hint.rs
+++ crates/data-structures/src/hint.rs
@@ -1,25 +1,4 @@
+#[cfg(not(feature = "nightly"))]
+pub use std::convert::{identity as likely, identity as unlikely};
 #[cfg(feature = "nightly")]
-pub use std::intrinsics::{likely, unlikely};
-
-#[cfg(not(feature = "nightly"))]
-#[inline(always)]
-#[cold]
-fn cold() {}
-
-#[cfg(not(feature = "nightly"))]
-#[inline(always)]
-pub fn likely(b: bool) -> bool {
-    if !b {
-        cold();
-    }
-    b
-}
-
-#[cfg(not(feature = "nightly"))]
-#[inline(always)]
-pub fn unlikely(b: bool) -> bool {
-    if b {
-        cold();
-    }
-    b
-}
+pub use std::hint::{likely, unlikely};
