06c28c19da8ea2b046a21a67e13db2cb755ba026
Oliver Nordbjerg <hi@notbjerg.me>
2025-08-17 08:15:17 +0200

    feat: lsp config and cli

--- crates/lsp/src/global_state.rs
+++ crates/lsp/src/global_state.rs
@@ -1,43 +1,41 @@
-use std::{ops::ControlFlow, sync::Arc};
+use std::{collections::HashMap, ops::ControlFlow, sync::Arc};
 
-use async_lsp::{ClientSocket, ResponseError};
+use async_lsp::{ClientSocket, LanguageClient, ResponseError};
 use lsp_types::{
     InitializeParams, InitializeResult, InitializedParams, LogMessageParams, MessageType,
-    ServerCapabilities, ServerInfo, TextDocumentSyncCapability, TextDocumentSyncKind,
-    TextDocumentSyncOptions, notification as notif,
+    PublishDiagnosticsParams, ServerInfo,
 };
 use solar_config::version::SHORT_VERSION;
-use solar_interface::data_structures::sync::RwLock;
+use solar_interface::{
+    Session,
+    data_structures::sync::RwLock,
+    diagnostics::{DiagCtxt, InMemoryEmitter},
+    source_map::FileName,
+};
+use solar_sema::Compiler;
+use tokio::task::JoinHandle;
 
-use crate::{NotifyResult, vfs::Vfs};
+use crate::{NotifyResult, config::negotiate_capabilities, proto, vfs::Vfs};
 
 pub(crate) struct GlobalState {
     client: ClientSocket,
     pub(crate) vfs: Arc<RwLock<Vfs>>,
+    analysis_version: usize,
 }
 
 impl GlobalState {
     pub(crate) fn new(client: ClientSocket) -> Self {
-        Self { client, vfs: Arc::new(Default::default()) }
+        Self { client, vfs: Arc::new(Default::default()), analysis_version: 0 }
     }
 
     pub(crate) fn on_initialize(
         &mut self,
-        _: InitializeParams,
+        params: InitializeParams,
     ) -> impl Future<Output = Result<InitializeResult, ResponseError>> + use<> {
+        let (capabilities, _config) = negotiate_capabilities(params);
+
         std::future::ready(Ok(InitializeResult {
-            capabilities: ServerCapabilities {
-                text_document_sync: Some(TextDocumentSyncCapability::Options(
-                    TextDocumentSyncOptions {
-                        open_close: Some(true),
-                        change: Some(TextDocumentSyncKind::INCREMENTAL),
-                        will_save: None,
-                        will_save_wait_until: None,
-                        ..Default::default()
-                    },
-                )),
-                ..Default::default()
-            },
+            capabilities,
             server_info: Some(ServerInfo {
                 name: "solar".into(),
                 version: Some(SHORT_VERSION.into()),
@@ -46,32 +44,97 @@ impl GlobalState {
     }
 
     pub(crate) fn on_initialized(&mut self, _: InitializedParams) -> NotifyResult {
-        self.info_msg("solar initialized");
+        let _ = self.client.log_message(LogMessageParams {
+            typ: MessageType::INFO,
+            message: "solar initialized".into(),
+        });
         ControlFlow::Continue(())
+    }
+
+    /// Parses, lowers, and performs analysis on project files, including in-memory only files.
+    ///
+    /// Each time analysis is triggered, a version is assigned to the analysis. A snapshot is then
+    /// taken of the global state ([`GlobalStateSnapshot`]) and analysis is performed on
+    /// the entire project in a separate thread.
+    ///
+    /// Currently, Solar is sufficiently fast at parsing and lowering even large Solidity projects,
+    /// so while analysing the entire project is relatively expensive compared to incremental
+    /// analysis, it is still fast enough for most workloads. A potential improvement would be to
+    /// enable incremental parsing and analysis in Solar using e.g. [`salsa`].
+    ///
+    /// [`salsa`]: https://docs.rs/salsa/latest/salsa/
+    pub(crate) fn recompute(&mut self) {
+        self.analysis_version += 1;
+        let version = self.analysis_version;
+        self.spawn_with_snapshot(move |mut snapshot| {
+            // todo: if this errors, we should notify the user
+            // todo: set base path to project root
+            // todo: remappings
+            let (emitter, diag_buffer) = InMemoryEmitter::new();
+            let sess = Session::builder().dcx(DiagCtxt::new(Box::new(emitter))).build();
+
+            let mut compiler = Compiler::new(sess);
+            let _ = compiler.enter_mut(move |compiler| -> solar_interface::Result<_> {
+                // Parse the files.
+                let mut parsing_context = compiler.parse();
+                // todo: unwraps
+                parsing_context.add_files(snapshot.vfs.read().iter().map(|(path, contents)| {
+                    compiler
+                        .sess()
+                        .source_map()
+                        .new_source_file(
+                            FileName::real(path.as_path().unwrap()),
+                            contents.to_string(),
+                        )
+                        .unwrap()
+                }));
+
+                parsing_context.parse();
+
+                // Perform lowering and analysis.
+                // We should never encounter `ControlFlow::Break` because we do not stop after
+                // parsing, so we ignore the return.
+                // todo: handle errors (currently this always errors?)
+                let _ = compiler.lower_asts();
+                let _ = compiler.analysis();
+
+                // todo handle diagnostic clearing
+                // todo clean this mess up boya
+                let diagnostics: HashMap<_, Vec<_>> = diag_buffer
+                    .read()
+                    .iter()
+                    .filter_map(|diag| proto::diagnostic(compiler.sess().source_map(), diag))
+                    .fold(HashMap::new(), |mut diags, (path, diag)| {
+                        diags.entry(path).or_default().push(diag);
+                        diags
+                    });
+                for (uri, diagnostics) in diagnostics.into_iter() {
+                    let _ = snapshot.client.publish_diagnostics(PublishDiagnosticsParams::new(
+                        uri,
+                        diagnostics,
+                        None,
+                    ));
+                }
+
+                Ok(())
+            });
+        });
+    }
+
+    fn snapshot(&self) -> GlobalStateSnapshot {
+        GlobalStateSnapshot { client: self.client.clone(), vfs: self.vfs.clone() }
+    }
+
+    fn spawn_with_snapshot<T: Send + 'static>(
+        &self,
+        f: impl FnOnce(GlobalStateSnapshot) -> T + Send + 'static,
+    ) -> JoinHandle<T> {
+        let snapshot = self.snapshot();
+        tokio::task::spawn_blocking(move || f(snapshot))
     }
 }
 
-impl GlobalState {
-    #[expect(unused)]
-    fn warn_msg(&self, message: impl Into<String>) {
-        let _ = self.client.notify::<notif::LogMessage>(LogMessageParams {
-            typ: MessageType::WARNING,
-            message: message.into(),
-        });
-    }
-
-    #[expect(unused)]
-    fn error_msg(&self, message: impl Into<String>) {
-        let _ = self.client.notify::<notif::LogMessage>(LogMessageParams {
-            typ: MessageType::ERROR,
-            message: message.into(),
-        });
-    }
-
-    fn info_msg(&self, message: impl Into<String>) {
-        let _ = self.client.notify::<notif::LogMessage>(LogMessageParams {
-            typ: MessageType::INFO,
-            message: message.into(),
-        });
-    }
+pub(crate) struct GlobalStateSnapshot {
+    client: ClientSocket,
+    vfs: Arc<RwLock<Vfs>>,
 }
