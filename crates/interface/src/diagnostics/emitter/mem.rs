fba6bd246c3eb5b94eb058c3e5a7e0fc5d2b0db8
Oliver Nordbjerg <hi@notbjerg.me>
2025-08-27 17:48:33 +0200

    feat: `InMemoryEmitter`

---
+++ crates/interface/src/diagnostics/emitter/mem.rs
@@ -0,0 +1,29 @@
+use std::sync::{Arc, RwLock};
+
+use crate::diagnostics::{Diag, Emitter};
+
+/// An in-memory diagnostics emitter.
+///
+/// Diagnostics are pushed to a shared buffer as-is.
+///
+/// # Warning
+///
+/// Do **NOT** hold a read lock on the buffer across compiler passes as this will prevent the
+/// compiler from pushing diagnostics.
+pub struct InMemoryEmitter {
+    buffer: Arc<RwLock<Vec<Diag>>>,
+}
+
+impl InMemoryEmitter {
+    /// Creates a new emitter, returning the emitter itself and the buffer.
+    pub fn new() -> (Self, Arc<RwLock<Vec<Diag>>>) {
+        let buffer = Default::default();
+        (Self { buffer: Arc::clone(&buffer) }, buffer)
+    }
+}
+
+impl Emitter for InMemoryEmitter {
+    fn emit_diagnostic(&mut self, diagnostic: &crate::diagnostics::Diag) {
+        self.buffer.write().unwrap().push(diagnostic.clone());
+    }
+}
