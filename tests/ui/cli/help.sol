5b317aed17af978824dae427257a94b4dbbddb52
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-08-28 11:01:24 +0200

    exe

--- tests/ui/cli/help.sol
+++ tests/ui/cli/help.sol
@@ -2,4 +2,4 @@
 //@[short] compile-flags: -h
 //@[long] compile-flags: --help
 //@ normalize-stdout-test: "\[default: \d+\]" -> "[default: <DEFAULT>]"
-//@ normalize-stdout-test: "solar(\.exe)?" -> "solar[EXE]"
+//@ normalize-stdout-test: "\.exe" -> ""
