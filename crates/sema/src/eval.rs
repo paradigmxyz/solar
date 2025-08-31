2844e95b63b07b6b59861ef965b94f117277c52e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-08-31 23:10:45 +0200

    chore: tweak eval error msg

--- crates/sema/src/eval.rs
+++ crates/sema/src/eval.rs
@@ -53,8 +53,9 @@ impl<'gcx> ConstantEvaluator<'gcx> {
         match err.kind {
             EE::AlreadyEmitted(guar) => guar,
             _ => {
-                let msg = "evaluation of constant value failed";
-                self.gcx.dcx().err(msg).span(expr.span).span_note(err.span, err.kind.msg()).emit()
+                let msg = format!("failed to evaluate constant: {}", err.kind.msg());
+                let label = "evaluation of constant value failed here";
+                self.gcx.dcx().err(msg).span(expr.span).span_label(err.span, label).emit()
             }
         }
     }
@@ -163,14 +164,16 @@ impl IntScalar {
     pub fn binop(&self, r: &Self, op: hir::BinOpKind) -> Result<Self, EE> {
         let l = self;
         Ok(match op {
-            hir::BinOpKind::Lt => Self::from_bool(l.data < r.data),
-            hir::BinOpKind::Le => Self::from_bool(l.data <= r.data),
-            hir::BinOpKind::Gt => Self::from_bool(l.data > r.data),
-            hir::BinOpKind::Ge => Self::from_bool(l.data >= r.data),
-            hir::BinOpKind::Eq => Self::from_bool(l.data == r.data),
-            hir::BinOpKind::Ne => Self::from_bool(l.data != r.data),
-            hir::BinOpKind::Or | hir::BinOpKind::BitOr => Self::new(l.data | r.data),
-            hir::BinOpKind::And | hir::BinOpKind::BitAnd => Self::new(l.data & r.data),
+            // hir::BinOpKind::Lt => Self::from_bool(l.data < r.data),
+            // hir::BinOpKind::Le => Self::from_bool(l.data <= r.data),
+            // hir::BinOpKind::Gt => Self::from_bool(l.data > r.data),
+            // hir::BinOpKind::Ge => Self::from_bool(l.data >= r.data),
+            // hir::BinOpKind::Eq => Self::from_bool(l.data == r.data),
+            // hir::BinOpKind::Ne => Self::from_bool(l.data != r.data),
+            // hir::BinOpKind::Or => Self::from_bool(l.data != 0 || r.data != 0),
+            // hir::BinOpKind::And => Self::from_bool(l.data != 0 && r.data != 0),
+            hir::BinOpKind::BitOr => Self::new(l.data | r.data),
+            hir::BinOpKind::BitAnd => Self::new(l.data & r.data),
             hir::BinOpKind::BitXor => Self::new(l.data ^ r.data),
             hir::BinOpKind::Shr => {
                 Self::new(l.data.wrapping_shr(r.data.try_into().unwrap_or(usize::MAX)))
@@ -195,6 +198,14 @@ impl IntScalar {
             }
             hir::BinOpKind::Div => Self::new(l.data.checked_div(r.data).ok_or(EE::DivisionByZero)?),
             hir::BinOpKind::Rem => Self::new(l.data.checked_rem(r.data).ok_or(EE::DivisionByZero)?),
+            hir::BinOpKind::Lt
+            | hir::BinOpKind::Le
+            | hir::BinOpKind::Gt
+            | hir::BinOpKind::Ge
+            | hir::BinOpKind::Eq
+            | hir::BinOpKind::Ne
+            | hir::BinOpKind::Or
+            | hir::BinOpKind::And => return Err(EE::UnsupportedBinaryOp.into()),
         })
     }
 }
@@ -203,10 +214,10 @@ impl IntScalar {
 pub enum EvalErrorKind {
     RecursionLimitReached,
     ArithmeticOverflow,
-    IntTooBig,
     DivisionByZero,
     UnsupportedLiteral,
     UnsupportedUnaryOp,
+    UnsupportedBinaryOp,
     UnsupportedExpr,
     NonConstantVar,
     AlreadyEmitted(ErrorGuaranteed),
@@ -222,13 +233,13 @@ impl EvalErrorKind {
         match self {
             Self::RecursionLimitReached => "recursion limit reached",
             Self::ArithmeticOverflow => "arithmetic overflow",
-            Self::IntTooBig => "integer value is too big",
-            Self::DivisionByZero => "division by zero",
+            Self::DivisionByZero => "attempted to divide by zero",
             Self::UnsupportedLiteral => "unsupported literal",
             Self::UnsupportedUnaryOp => "unsupported unary operation",
+            Self::UnsupportedBinaryOp => "unsupported binary operation",
             Self::UnsupportedExpr => "unsupported expression",
             Self::NonConstantVar => "only constant variables are allowed",
-            Self::AlreadyEmitted(_) => "error already emitted",
+            Self::AlreadyEmitted(_) => unreachable!(),
         }
     }
 }
