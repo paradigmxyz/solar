a363974cb929de6fb5eba28f0da9ca33b92a52db
Oliver Nordbjerg <hi@notbjerg.me>
2025-12-15 17:54:32 +0100

    test: add bytes and contract type to mapping key type check test

--- tests/ui/typeck/mapping_key_type_check.sol
+++ tests/ui/typeck/mapping_key_type_check.sol
@@ -18,9 +18,11 @@ library L{
 contract C {
     mapping(uint => uint) m0;
     mapping(string => uint) m1;
+    mapping(bytes => uint) m1b;
     mapping(E => uint) m2;
     mapping(U => uint) m3;
     mapping(L.E1 => uint) m4;
+    mapping(C => uint) m4b;
     mapping(L.S1 => uint) m5; //~ ERROR: only elementary types, user defined value types, contract types or enums are allowed as mapping keys.
     mapping(uint[] => uint) m6; //~ ERROR: only elementary types, user defined value types, contract types or enums are allowed as mapping keys.
 }
