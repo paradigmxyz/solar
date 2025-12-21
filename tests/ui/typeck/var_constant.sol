be06e5efa14907cd88bf4984ce494f5baa7c55a4
Joydeep Singha <joydeepsingha68@gmail.com>
2025-12-22 02:33:57 +0530

    fix(typeck): update error messages for constant variable initializers to clarify compile-time requirements

--- tests/ui/typeck/var_constant.sol
+++ tests/ui/typeck/var_constant.sol
@@ -4,8 +4,8 @@
 contract TestConstants {
     // Constant with non-constant initializers - ERROR
     uint nonConstant;
-    uint constant refNonConstant = nonConstant; //~ ERROR: failed to evaluate constant: only constant variables are allowed
+    uint constant refNonConstant = nonConstant; //~ ERROR: initial value for constant variable has to be compile-time constant
 
-    uint constant blockTime = block.timestamp; //~ ERROR: failed to evaluate constant: unsupported expression
+    uint constant blockTime = block.timestamp; //~ ERROR: initial value for constant variable has to be compile-time constant
     //~^ ERROR: mismatched types
 }
