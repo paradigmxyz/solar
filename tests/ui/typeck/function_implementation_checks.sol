4271b358b67d2a085750d2a2400e54efb81627ee
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-01-14 12:46:26 +0000

    test: add unimplemented constructor test

--- tests/ui/typeck/function_implementation_checks.sol
+++ tests/ui/typeck/function_implementation_checks.sol
@@ -1,14 +1,11 @@
-// Error 5424: functions without implementation must be marked virtual
 contract A {
     function f() public; //~ ERROR: functions without implementation must be marked virtual
 }
 
-// Error 3942: virtual and private cannot be used together
 contract B {
     function f() private virtual {} //~ ERROR: "virtual" and "private" cannot be used together
 }
 
-// Error 9231: library functions must be implemented if declared
 library L {
     function f() public; //~ ERROR: library functions must be implemented if declared
 }
