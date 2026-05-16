38cdbeee39881b68c4a16a9c581551264393953e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-05-16 15:32:59 +0200

    test(parser): simplify pow ui regression

--- tests/ui/typeck/pow_associativity.sol
+++ tests/ui/typeck/pow_associativity.sol
@@ -1,11 +1,9 @@
 //@compile-flags: -Ztypeck
 
-contract C {
-    function exponentiationRightAssociative(uint[2**3**2] memory a) internal pure {
-        uint[512] memory b = a;
-    }
+function test() pure {
+    uint[2**3**2] memory a;
+    uint[512] memory b = a;
 
-    function subtractionLeftAssociative(uint[8 - 3 - 2] memory a) internal pure {
-        uint[3] memory b = a;
-    }
+    uint[8 - 3 - 2] memory c;
+    uint[3] memory d = c;
 }
