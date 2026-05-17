5f0e8113c625845520d3d278498e448896c06009
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-05-18 01:22:16 +0200

    test(sema): annotate imported operator errors

--- tests/ui/using-for/imports/imported_non_global_operator_definition.sol
+++ tests/ui/using-for/imports/imported_non_global_operator_definition.sol
@@ -1,14 +1,15 @@
 //@compile-flags: -Ztypeck
-// check-fail
+//@ error-in-other-file: operators can only be defined in a global
+//@ error-in-other-file: operators can only be defined in a global
 
 import {DefinedInt} from "./auxiliary/defined_non_global_operator.sol";
 
 contract C {
     function binary(DefinedInt a, DefinedInt b) public pure returns (DefinedInt) {
-        return a + b;
+        return a + b; //~ ERROR: cannot apply builtin operator
     }
 
     function unary(DefinedInt a) public pure returns (DefinedInt) {
-        return -a;
+        return -a; //~ ERROR: cannot apply unary operator
     }
 }
--- tests/ui/using-for/imports/transitive_non_global_operator.sol
+++ tests/ui/using-for/imports/transitive_non_global_operator.sol
@@ -1,15 +1,18 @@
 //@compile-flags: -Ztypeck
-// check-fail
+//@ error-in-other-file: operators can only be defined in a global
+//@ error-in-other-file: operators can only be defined in a global
+//@ error-in-other-file: operators can only be defined in a global
+//@ error-in-other-file: operators can only be defined in a global
 
 import "./auxiliary/non_global_left.sol";
 import "./auxiliary/non_global_right.sol";
 
 contract C {
     function binary(TransitiveInt a, TransitiveInt b) public pure returns (TransitiveInt) {
-        return a + b;
+        return a + b; //~ ERROR: cannot apply builtin operator
     }
 
     function unary(TransitiveInt a) public pure returns (TransitiveInt) {
-        return -a;
+        return -a; //~ ERROR: cannot apply unary operator
     }
 }
