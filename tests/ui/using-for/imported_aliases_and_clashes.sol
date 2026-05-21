d864d407c2dfbdf57ac2d62da2a701b153e190e1
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-05-21 04:24:37 +0200

    test: rename using alias clash coverage

--- tests/ui/using-for/imported_aliases_and_clashes.sol
+++ tests/ui/using-for/imported_aliases_and_clashes.sol
@@ -2,8 +2,8 @@
 // ported-from: test/libsolidity/semanticTests/using/imported_functions.sol
 // ported-from: test/libsolidity/syntaxTests/using/global_local_clash.sol
 
-import {S, f1 as f, gen, inc as aliasedInc} from "./auxiliary/regressions_imports.sol";
-import "./auxiliary/regressions_imports.sol" as Imports;
+import {S, f1 as f, gen, inc as aliasedInc} from "./auxiliary/imported_aliases_and_clashes.sol";
+import "./auxiliary/imported_aliases_and_clashes.sol" as Imports;
 
 using {Imports.inc, aliasedInc} for uint256;
 
