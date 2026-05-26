c4dae9ce1b1c74ab98007fb105bb582119c3ce86
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-05-26 14:35:49 +0200

    ci: copy reth llvm install scripts

--- .github/scripts/install_llvm_brew.sh
+++ .github/scripts/install_llvm_brew.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 v=${1:-22}
 
 brew install "llvm@${v}"
-prefix="$(brew --prefix llvm@${v})"
+prefix="$(brew --prefix "llvm@${v}")"
 echo "LLVM_SYS_${v}1_PREFIX=${prefix}" >> "$GITHUB_ENV"
 echo "${prefix}/bin" >> "$GITHUB_PATH"
 
 echo "LLVM $v installed:"
-"${prefix}/bin/llvm-config" --version --bindir --libdir
+"${prefix}/bin/llvm-config" --version
