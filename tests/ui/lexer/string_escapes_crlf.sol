50813337ef556a0f72ce764de5ffd3e75d66e4dd
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-06-04 19:21:24 +0200

    test: update string recovery diagnostics

--- tests/ui/lexer/string_escapes_crlf.sol
+++ tests/ui/lexer/string_escapes_crlf.sol
@@ -3,8 +3,10 @@
 // Escaped - OK
 string constant s = "\
 ";
+//~v ERROR: identifier `s` already declared
 string constant s = unicode"\
 ";
+//~v ERROR: identifier `s` already declared
 bytes constant s = hex"\
 ";
 //~^^ ERROR: invalid hex digit
@@ -13,12 +15,14 @@ bytes constant s = hex"\
 // 3x for \\, \r, \n
 
 // Escaped, but can only escape one newline
+//~v ERROR: identifier `s` already declared
 string constant s = "\
 
 "; //~^ ERROR: cannot skip multiple lines
 string constant s = unicode"\
 
 "; //~^ ERROR: cannot skip multiple lines
+//~^^^ ERROR: identifier `s` already declared
 bytes constant s = hex"\
 
 ";
@@ -27,15 +31,19 @@ bytes constant s = hex"\
 //~| ERROR: invalid hex digit
 //~^^^^^ ERROR: invalid hex digit
 //~| ERROR: invalid hex digit
+//~^^^^^^^^ ERROR: identifier `s` already declared
 // 5x for \\, \r, \n, \r, \n
 
 // Unescaped
+//~v ERROR: identifier `s` already declared
 string constant s = "
 "; //~^ ERROR: unescaped newline
 string constant s = unicode"
 "; //~^ ERROR: unescaped newline
+//~^^ ERROR: identifier `s` already declared
 bytes constant s = hex"
 ";
 //~^^ ERROR: invalid hex digit
 //~| ERROR: invalid hex digit
+//~^^^^ ERROR: identifier `s` already declared
 // 2x for \r, \n
--- tests/ui/lexer/string_escapes_lf.sol
+++ tests/ui/lexer/string_escapes_lf.sol
@@ -1,8 +1,10 @@
 // Escaped - OK
 string constant s = "\
 ";
+//~v ERROR: identifier `s` already declared
 string constant s = unicode"\
 ";
+//~v ERROR: identifier `s` already declared
 bytes constant s = hex"\
 ";
 //~^^ ERROR: invalid hex digit
@@ -10,26 +12,32 @@ bytes constant s = hex"\
 // 2 for \\, \n
 
 // Escaped, but can only escape one newline
+//~v ERROR: identifier `s` already declared
 string constant s = "\
 
 "; //~^ ERROR: cannot skip multiple lines
 string constant s = unicode"\
 
 "; //~^ ERROR: cannot skip multiple lines
+//~^^^ ERROR: identifier `s` already declared
 bytes constant s = hex"\
 
 ";
 //~^^^ ERROR: invalid hex digit
 //~| ERROR: invalid hex digit
 //~^^^^ ERROR: invalid hex digit
+//~^^^^^^ ERROR: identifier `s` already declared
 // 3x for \\, \n, \n
 
 // Unescaped
+//~v ERROR: identifier `s` already declared
 string constant s = "
 "; //~^ ERROR: unescaped newline
 string constant s = unicode"
 "; //~^ ERROR: unescaped newline
+//~^^ ERROR: identifier `s` already declared
 bytes constant s = hex"
 ";
 //~^^ ERROR: invalid hex digit
+//~^^^ ERROR: identifier `s` already declared
 // 1x for \n
