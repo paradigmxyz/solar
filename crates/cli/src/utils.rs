9cc4892bcd4d4cd0df4ee1a048ff72f1be8a406b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-06-04 23:31:39 +0200

    fix: limit warning codes to solc ids

--- crates/cli/src/utils.rs
+++ crates/cli/src/utils.rs
@@ -1,9 +1,6 @@
 //! Utility functions used by the Solar CLI.
 
 use solar_interface::diagnostics::DiagCtxt;
-#[cfg(not(feature = "tracing"))]
-use solar_interface::diagnostics::DiagId;
-#[cfg(feature = "tracing")]
 use std::io;
 
 #[cfg(feature = "tracing")]
@@ -77,15 +74,14 @@ impl<'a> tracing_subscriber::fmt::MakeWriter<'a> for LogDestination {
 pub fn init_logger(dst: LogDestination) -> impl Sized {
     #[cfg(not(feature = "tracing"))]
     {
-        let _ = dst;
         if std::env::var_os("RUST_LOG").is_some() {
             let msg = "`RUST_LOG` is set, but \"tracing\" support was not enabled at compile time";
-            DiagCtxt::new_early().warn(msg).code(DiagId::new_str("solar-tracing-disabled")).emit();
+            DiagCtxt::new_early().warn(msg).emit();
         }
         if std::env::var_os("SOLAR_PROFILE").is_some() {
             let msg =
                 "`SOLAR_PROFILE` is set, but \"tracing\" support was not enabled at compile time";
-            DiagCtxt::new_early().warn(msg).code(DiagId::new_str("solar-tracing-disabled")).emit();
+            DiagCtxt::new_early().warn(msg).emit();
         }
     }
 
