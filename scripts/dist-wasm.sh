be5cf4927c7d2f715d9cd30f26c5ee7355156c27
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-06-13 19:49:38 +0200

    chore: build wasm with minsize profile

--- scripts/dist-wasm.sh
+++ scripts/dist-wasm.sh
@@ -6,11 +6,11 @@ cd "$root"
 
 rustup target add wasm32-unknown-unknown
 export RUSTFLAGS="${RUSTFLAGS:+$RUSTFLAGS }-C link-arg=--export-table -C link-arg=--growable-table"
-cargo build -p solar-cli --lib --release --no-default-features --target wasm32-unknown-unknown
+cargo build -p solar-cli --lib --profile minsize --no-default-features --target wasm32-unknown-unknown
 
 out_dir="target/dist/solar-soljson"
 mkdir -p "$out_dir"
-cp target/wasm32-unknown-unknown/release/solar_cli.wasm "$out_dir/solar.wasm"
+cp target/wasm32-unknown-unknown/minsize/solar_cli.wasm "$out_dir/solar.wasm"
 cp crates/cli/soljson.js "$out_dir/soljson.js"
 
 tar -C target/dist -czf target/dist/solar-soljson.tar.gz solar-soljson
