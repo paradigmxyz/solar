1a46a9b956e871ead62542abd1515b1492bda732
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-06-16 20:25:34 +0200

    chore: dont need this anymore

--- crates/data-structures/src/fmt.rs
+++ crates/data-structures/src/fmt.rs
@@ -2,42 +2,6 @@ use std::{cell::Cell, fmt};
 
 pub use fmt::*;
 
-/// Wrapper for [`fmt::from_fn`].
-#[cfg(feature = "nightly")]
-pub fn from_fn<F: Fn(&mut fmt::Formatter<'_>) -> fmt::Result>(
-    f: F,
-) -> impl fmt::Debug + fmt::Display {
-    fmt::from_fn(f)
-}
-
-/// Polyfill for [`fmt::from_fn`].
-#[cfg(not(feature = "nightly"))]
-pub fn from_fn<F: Fn(&mut fmt::Formatter<'_>) -> fmt::Result>(
-    f: F,
-) -> impl fmt::Debug + fmt::Display {
-    struct FromFn<F>(F);
-
-    impl<F> fmt::Debug for FromFn<F>
-    where
-        F: Fn(&mut fmt::Formatter<'_>) -> fmt::Result,
-    {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            (self.0)(f)
-        }
-    }
-
-    impl<F> fmt::Display for FromFn<F>
-    where
-        F: Fn(&mut fmt::Formatter<'_>) -> fmt::Result,
-    {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            (self.0)(f)
-        }
-    }
-
-    FromFn(f)
-}
-
 /// Returns `list` formatted as a comma-separated list with "or" before the last item.
 pub fn or_list<I>(list: I) -> impl fmt::Display
 where
