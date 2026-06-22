fbf5598db4d3838e4956d22c3e2f0c9a088ff1b0
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-06-22 21:50:27 +0200

    cleanup

--- examples/src/Counter.sol
+++ examples/src/Counter.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
-    
+
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
     function increment() public {
-       number++;
+        number++;
     }
 }
