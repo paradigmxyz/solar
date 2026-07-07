c963ef4907522d3bb191aff53e4c98dc65506a30
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-07-07 21:09:08 +0200

    chore: fix changelog script

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -13,6 +13,7 @@ run_unless_dry_run() {
 root=$WORKSPACE_ROOT
 crate=$CRATE_ROOT
 crate_glob="${crate#"$root/"}/**"
+root_changelog="$root/CHANGELOG.md"
 
 if [[ "$crate" != *crates/* ]]; then
     echo "skipping $crate" >&2
@@ -23,13 +24,12 @@ if [ -n "$NO_GIT_CLIFF" ]; then
     exit 0
 fi
 
-command=(git cliff --workdir "$root" --config "$root/cliff.toml" --unreleased "${@}")
-if [ -z "$(git -C "$root" status --porcelain -- CHANGELOG.md)" ]; then
-    pushd "$root" >/dev/null
-    run_unless_dry_run "${command[@]}" --prepend "$root/CHANGELOG.md"
-    popd >/dev/null
+command=(git cliff --config "$root/cliff.toml" --unreleased "${@}")
+pushd "$root" >/dev/null
+if [ -z "$(git status --porcelain -- CHANGELOG.md)" ]; then
+    run_unless_dry_run "${command[@]}" --prepend "$root_changelog"
 else
-    echo "$root/CHANGELOG.md has already been generated" >&2
+    echo "$root_changelog has already been generated" >&2
 fi
 if [ -n "$crate" ] && [ "$root" != "$crate" ]; then
     crate_changelog="$crate/CHANGELOG.md"
@@ -37,5 +37,10 @@ if [ -n "$crate" ] && [ "$root" != "$crate" ]; then
         echo "missing changelog: $crate_changelog" >&2
         exit 1
     fi
-    run_unless_dry_run "${command[@]}" --include-path "$crate_glob" --prepend "$crate_changelog"
+    if [ "$(realpath "$crate_changelog")" = "$(realpath "$root_changelog")" ]; then
+        echo "$crate_changelog uses $root_changelog" >&2
+    else
+        run_unless_dry_run "${command[@]}" --include-path "$crate_glob" --prepend "$crate_changelog"
+    fi
 fi
+popd >/dev/null
