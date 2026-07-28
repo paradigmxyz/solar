dd8a418a5faabe17addc4af3f14eeafe7e4acd19
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-07-28 04:21:33 +0200

    fix(codegen): match precompile targets

--- tests/ui/codegen/lowering/precompile_builtins.sol
+++ tests/ui/codegen/lowering/precompile_builtins.sol
@@ -1,7 +1,16 @@
-//@ run-call: recover() => 0x7e5f4552091a69125d5dfcb7b8c2659029395bdf
-//@ run-call: recoverInvalid() => 0x0000000000000000000000000000000000000000
-//@ run-call: sha() => 0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
-//@ run-call: ripemd() => 0x8eb208f7e05d987a9b044a8e98c6b087f15a0bfc
+//@ revisions: homestead byzantium constantinople osaka
+//@[homestead] compile-flags: --evm-version homestead
+//@[byzantium] compile-flags: --evm-version byzantium
+//@[constantinople] compile-flags: --evm-version constantinople
+//@[osaka] compile-flags: --evm-version osaka
+//@[constantinople] run-call: recover() => 0x7e5f4552091a69125d5dfcb7b8c2659029395bdf
+//@[constantinople] run-call: recoverInvalid() => 0x0000000000000000000000000000000000000000
+//@[constantinople] run-call: sha() => 0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
+//@[constantinople] run-call: ripemd() => 0x8eb208f7e05d987a9b044a8e98c6b087f15a0bfc
+//@[osaka] run-call: recover() => 0x7e5f4552091a69125d5dfcb7b8c2659029395bdf
+//@[osaka] run-call: recoverInvalid() => 0x0000000000000000000000000000000000000000
+//@[osaka] run-call: sha() => 0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
+//@[osaka] run-call: ripemd() => 0x8eb208f7e05d987a9b044a8e98c6b087f15a0bfc
 
 contract PrecompileBuiltins {
     function recover() external pure returns (address) {
