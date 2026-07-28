3f3e01ba69e43f3425ce6ca7b62fc0820b3ff184
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-07-28 05:54:09 +0200

    test(codegen): simplify precompile revisions

--- tests/ui/codegen/lowering/precompile_builtins.sol
+++ tests/ui/codegen/lowering/precompile_builtins.sol
@@ -3,10 +3,10 @@
 //@[byzantium] compile-flags: --evm-version byzantium
 //@[constantinople] compile-flags: --evm-version constantinople
 //@[osaka] compile-flags: --evm-version osaka
-//@[homestead,byzantium,constantinople,osaka] run-call: recover() => 0x7e5f4552091a69125d5dfcb7b8c2659029395bdf
-//@[homestead,byzantium,constantinople,osaka] run-call: recoverInvalid() => 0x0000000000000000000000000000000000000000
-//@[homestead,byzantium,constantinople,osaka] run-call: sha() => 0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
-//@[homestead,byzantium,constantinople,osaka] run-call: ripemd() => 0x8eb208f7e05d987a9b044a8e98c6b087f15a0bfc
+//@ run-call: recover() => 0x7e5f4552091a69125d5dfcb7b8c2659029395bdf
+//@ run-call: recoverInvalid() => 0x0000000000000000000000000000000000000000
+//@ run-call: sha() => 0xba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
+//@ run-call: ripemd() => 0x8eb208f7e05d987a9b044a8e98c6b087f15a0bfc
 
 contract PrecompileBuiltins {
     function recover() external pure returns (address) {
