2285f258aa1d447bb3b319dabc97861b86f763f2
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-07-30 20:46:49 +0200

    style(codegen): format main merge

--- crates/codegen/src/lower/mod.rs
+++ crates/codegen/src/lower/mod.rs
@@ -1340,8 +1340,7 @@ impl<'gcx> Lowerer<'gcx> {
                         MemoryObjectKind::Struct,
                     );
                     builder.mstore(offset_val, value);
-                } else if let Some(value) =
-                    self.lower_bulk_zero_return_struct(&mut builder, ret_id)
+                } else if let Some(value) = self.lower_bulk_zero_return_struct(&mut builder, ret_id)
                 {
                     builder.mstore(offset_val, value);
                 } else if let Some(value) = self.lower_default_variable_value(&mut builder, ret_id)
