86b86f16a55f2f550015fc68add6cd6888c04aea
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-08-02 06:58:17 +0200

    test(codegen): check hashed ABI encode

--- tests/ui/codegen/run-call/abi_encode_roundtrip.sol
+++ tests/ui/codegen/run-call/abi_encode_roundtrip.sol
@@ -1,7 +1,12 @@
 //@ run-call: roundtrip 7 => 7
+//@ run-call: hash 7 => 0xa66cc928b5edb82af9bd49922954155ab7b0942694bea4ce44661d9a8736c688
 
 contract AbiEncodeRoundtrip {
     function roundtrip(uint256 value) external pure returns (uint256) {
         return abi.decode(abi.encode(value), (uint256));
+    }
+
+    function hash(uint256 value) external pure returns (bytes32) {
+        return keccak256(abi.encode(value));
     }
 }
