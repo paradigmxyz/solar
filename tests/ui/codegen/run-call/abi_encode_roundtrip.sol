7e4c17157bc667ca7b2807123797720929d710b9
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-08-05 11:09:19 +0200

    test(codegen): cover nested ABI roundtrip

--- tests/ui/codegen/run-call/abi_encode_roundtrip.sol
+++ tests/ui/codegen/run-call/abi_encode_roundtrip.sol
@@ -3,6 +3,7 @@
 //@ run-call: wordsRoundtrip [1, 2, 3] => 6
 //@ run-call: nestedWordsRoundtrip [[1, 2], [3]] => 3
 //@ run-call: mixedRoundtrip() => 9, 3, 3
+//@ run-call: nestedMixedRoundtrip() => 2, 8, 3, 4
 //@ run-call: hash 7 => 0xa66cc928b5edb82af9bd49922954155ab7b0942694bea4ce44661d9a8736c688
 
 contract AbiEncodeRoundtrip {
@@ -32,6 +33,19 @@ contract AbiEncodeRoundtrip {
         (uint256[2] memory decodedNumbers, bytes memory decodedTail, uint8 flag) =
             abi.decode(abi.encode(numbers, tail, uint8(3)), (uint256[2], bytes, uint8));
         return (decodedNumbers[1], decodedTail.length, flag);
+    }
+
+    function nestedMixedRoundtrip() external pure returns (uint256, uint256, uint256, uint256) {
+        uint256[][] memory matrix = new uint256[][](2);
+        matrix[0] = new uint256[](2);
+        matrix[0][0] = 7;
+        matrix[0][1] = 8;
+        matrix[1] = new uint256[](1);
+        matrix[1][0] = 3;
+        bytes memory tail = hex"01020304";
+        (uint256[][] memory decodedMatrix, bytes memory decodedTail) =
+            abi.decode(abi.encode(matrix, tail), (uint256[][], bytes));
+        return (decodedMatrix.length, decodedMatrix[0][1], decodedMatrix[1][0], decodedTail.length);
     }
 
     function hash(uint256 value) external pure returns (bytes32) {
