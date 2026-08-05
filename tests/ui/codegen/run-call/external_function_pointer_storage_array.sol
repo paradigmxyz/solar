aa0e35643d90e5362e40114a20d168362f381c7d
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-08-05 15:41:42 +0200

    test(codegen): cover calldata pointer storage

--- tests/ui/codegen/run-call/external_function_pointer_storage_array.sol
+++ tests/ui/codegen/run-call/external_function_pointer_storage_array.sol
@@ -1,4 +1,4 @@
-//@ run-call: runAll() => "[a called][b called][a called][b called]"
+//@ run-call: runAll() => "[a called][b called][a called][b called][a called][b called]"
 // ported-from: test/libsolidity/semanticTests/array/copying/function_type_array_to_storage.sol
 
 contract ExternalFunctionPointerStorageArray {
@@ -14,11 +14,18 @@ contract ExternalFunctionPointerStorageArray {
         log = string.concat(log, "[b called]");
     }
 
+    function storeCalldata(function() external[] calldata pointers) external {
+        fs = pointers;
+    }
+
     function runAll() external returns (string memory) {
         function() external[] memory pointers = new function() external[](2);
         pointers[0] = this.a;
         pointers[1] = this.b;
         fs = pointers;
+        fs[0]();
+        fs[1]();
+        this.storeCalldata(pointers);
         fs[0]();
         fs[1]();
         gs = fs;
