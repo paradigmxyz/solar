add553f878fe822e29b897b181f16a0062ca98b9
malik672 <aremumalik05@gmail.com>
2026-08-14 06:48:27 +0100

    test(codegen): fix stack phi check

--- tests/ui/codegen/lowering/acyclic_stack_phi.sol
+++ tests/ui/codegen/lowering/acyclic_stack_phi.sol
@@ -8,7 +8,7 @@ contract AcyclicStackPhi {
     // CHECK-NEXT: push {{[a-z0-9]+}}
     // CHECK-NEXT: jumpi
     // CHECK: push 4
-    // CHECK-NEXT: push 4
+    // CHECK-NEXT: dup1
     // CHECK-NEXT: calldataload
     // CHECK-NEXT: add
     // CHECK-NEXT: calldataload
