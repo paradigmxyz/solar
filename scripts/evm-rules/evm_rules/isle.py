9e07ec240493205afd5a11a30ee8ba6ef95ec70f
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-09-17 16:35:00 +0000

    style: format merged rule predicate list

--- scripts/evm-rules/evm_rules/isle.py
+++ scripts/evm-rules/evm_rules/isle.py
@@ -357,7 +357,8 @@ class Context:
                 ]
             )
             return shift
-        if name in ("differ",
+        if name in (
+            "differ",
             "has_bitwise_shifting",
             "has_self_balance",
             "in_current_block",
